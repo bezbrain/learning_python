7b1ca58380204a8544c88fe1b99bca199e094e49
bezbrain <belloquadriolawale@gmail.com>
2025-04-21 20:10:13 +0100

    feat: learning list array data structure

---
+++ 07-List-data-structure/python.py
@@ -0,0 +1,11 @@
+# The list data structure is an array
+
+states_in_nigeria = ["Abia", "Kwara", "Bauchi", "Ondo", "Lagos", "Ekiti"]
+
+states_in_nigeria[1] = "Taraba"
+
+states_in_nigeria.append("Ondo")  # To add an item to the end of the list
+
+states_in_nigeria.extend(["Osun", "Oyo"])  # To add items to the end of the list
+
+print(states_in_nigeria)
