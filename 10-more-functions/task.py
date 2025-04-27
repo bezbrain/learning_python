110b6f7563044ff07041dce461f2b9243987608d
bezbrain <belloquadriolawale@gmail.com>
2025-04-27 15:14:00 +0100

    feat: leap year task

--- 10-more-functions/task.py
+++ 10-more-functions/task.py
@@ -9,7 +9,13 @@
 
 
 def is_leap_year(year):
-    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    if year % 400 == 0:
         return True
-    else:
+    if year % 100 == 0:
         return False
+    if year % 4 == 0:
+        return True
+    return False
+
+
+print(is_leap_year(2024))
