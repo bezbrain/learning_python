46ae262661133fbc47196d07e948bcd814786e02
bezbrain <belloquadriolawale@gmail.com>
2025-04-27 23:26:17 +0100

    feat: doc string

--- 10-more-functions/function-with-output.py
+++ 10-more-functions/function-with-output.py
@@ -1,4 +1,6 @@
+# Line 3 is an example of docString. It is a form of brief documentation about our function and it is always written immediately after our function declaration. When you hover on function in line 8, we will see the documentation displayed
 def function_with_output(sentence):
+    """Format a sentence to be capitalized and print the title case"""
     print(sentence.capitalize())
     print(sentence.title())
 
