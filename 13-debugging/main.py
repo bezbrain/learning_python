369d6d2b9c48809c3148a32fb9e769c5737efadd
bezbrain <belloquadriolawale@gmail.com>
2025-05-04 22:11:23 +0100

    feat: debugging

---
+++ 13-debugging/main.py
@@ -0,0 +1,13 @@
+try:
+    age = int(input("How old are you? "))
+except ValueError:
+    print(
+        "You have typed in an invalid number. Please try again with a numerical value"
+    )
+    age = int(input("How old are you? "))
+
+
+if age > 18:
+    print(f"You can drive at age {age}")
+else:
+    print(f"You cannot drive at age {age}")
