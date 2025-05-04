55c94433f13062a5449dea3509bb3886d1d3c487
bezbrain <belloquadriolawale@gmail.com>
2025-05-04 19:45:50 +0100

    feat: learning global constants

---
+++ 12-Scopes-in-Python/constant-and-global-scope.py
@@ -0,0 +1,11 @@
+# Global constants are values that are global and you don't plan to change. The naming convention is to us uppercase
+
+PI = 3.142
+GOOGLE_URL = "https://www.google.com"
+
+
+def my_func():
+    print(f"This is the url: {GOOGLE_URL}")
+
+
+my_func()
